5eec9566a02c5fdc68a3279d6c9a124917338a89
Alexander Pastukhov <aib78@mail.ru>
2017-12-17 15:11:31 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,4 +1,11 @@
 package ru.job4j;
+/**
+* конструктор вывода строки в консоль.
+* @param arg - arg.
+* @author pastukhov
+* @since 17.12.2017
+* @version 1
+*/
 
 public class Calculate {
 	public static void main(String[] args){
