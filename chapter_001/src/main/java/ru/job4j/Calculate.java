deecd6ff70ebc17e152121f1b275809f5b4d554d
Alexander Pastukhov <aib78@mail.ru>
2017-12-20 10:45:50 +0300

    text

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,21 +1,28 @@
 package ru.job4j;
+
 /**
-* конструктор вывода строки в консоль.
-* @param arg - arg.
-* @author pastukhov
-* @since 17.12.2017
-* @version 1
-*/
+ * конструктор вывода строки в консоль.
+ *
+ * @param arg - arg.
+ * @author pastukhov
+ * @version 1
+ * @since 17.12.2017
+ */
 
 public class Calculate {
-	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-	public String echo(String name) {
-	    return "Echo, echo, echo : " + name;
-	}
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 
 }
 	
--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -1,28 +1,31 @@
 package ru.job4j;
 
 import org.junit.Test;
+
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Alexander Pastukhov (aib78@mail.ru)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Alexander Pastukhov (aib78@mail.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
-/**
-* Test echo.
-*/ @Test
-public void whenTakeNameThenTreeEchoPlusName() {
-    String input = "Alexandr Pastukhov";
-    String expect = "Echo, echo, echo : Alexandr Pastukhov"; 
-    Calculate calc = new Calculate();
-    String result = calc.echo(input);
-    assertThat(result, is(expect));
-}
- 
+    /**
+     * Test echo.
+     */
+    @Test
+    public void whenTakeNameThenTreeEchoPlusName() {
+        String input = "Alexandr Pastukhov";
+        String expect = "Echo, echo, echo : Alexandr Pastukhov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+
 }
